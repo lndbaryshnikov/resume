b0f00baf9a23b23b0267fc79ecec9bf1d3ddaca7
Leonid <leobar98@mail.ru>
2019-03-21 19:07:13 +0300

    configure webpack.config.js for html and js

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+    src: path.join(__dirname, 'src/basic'),
+    dist: path.join(__dirname, 'dist')
+};
+
+module.exports = {
+    entry: PATHS.src + '/index.js',
+    output: {
+        path: PATHS.dist,
+        filename: '[name].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Resume'
+        })
+    ]
+};
